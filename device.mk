a9899fee882d72552e323d53f8aedf2bc5adaa53
Alone0316 <rutujamandlik06@gmail.com>
2023-02-08 10:24:46 +0000

    ruby: Build Bluetooth interface

--- device.mk
+++ device.mk
@@ -60,6 +60,11 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     tinymix
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor
+
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
