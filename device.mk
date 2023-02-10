ff02447048617a3d56efbea6142fb71c0360f967
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-02-10 13:11:31 +0000

    ruby: Build dummy HIDL libs

--- device.mk
+++ device.mk
@@ -96,6 +96,13 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
 # Keymaster
 PRODUCT_PACKAGES += \
     libkeymaster4.vendor \
