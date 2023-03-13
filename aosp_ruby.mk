007a83de16d88adc7f7e6874dcb0ef4571c02984
Alone0316 <rutujamandlik06@gmail.com>
2023-03-13 08:45:44 +0000

    tmp

--- aosp_ruby.mk
+++ aosp_ruby.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/ruby/device.mk)
 
 # Inherit some common LineageOS Stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_ruby
+PRODUCT_NAME := aosp_ruby
 PRODUCT_DEVICE := ruby
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
